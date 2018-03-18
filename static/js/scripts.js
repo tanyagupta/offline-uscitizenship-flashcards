0d7f91fda09c23d40151298445b05a747bd364b3
tanyagupta <gupta.tanya@gmail.com>
2018-03-18 09:44:13 -0400

    latest changes before new engine

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,6 +7,23 @@ var start_time = new Date();
 var  intervalID;// = window.setInterval(display_time, 1000);
 var times=0;
 
+const START =0;
+const END = 99;
+var questions_not_shown=0
+var the_pointers=[]
+for (var i=START+1;i<=END;i++) {
+   the_pointers.push(i);
+   questions_not_shown+= ","+i;
+ }
+
+ sessionStorage.setItem ('all_questions', questions_not_shown);
+ sessionStorage.setItem ('questions_not_shown', questions_not_shown);
+ //console.log(sessionStorage.getItem ('questions_not_shown'))
+
+ sessionStorage.setItem("random_counter","0")
+
+ sessionStorage.setItem("current_display_index","0");
+ sessionStorage.setItem("questions_shown","");
 
 
 /* Fetch from our API */
@@ -166,22 +183,7 @@ dbPromise.then(function(db){
 
 }).then(function(data){
 
- const START =0;
- const END = data.length -1;
- var questions_not_shown=0
- var the_pointers=[]
- for (var i=START+1;i<=END;i++) {
-    the_pointers.push(i);
-    questions_not_shown+= ","+i;
-  }
 
-sessionStorage.setItem ('questions_not_shown', questions_not_shown);
-
-sessionStorage.setItem ("dif_counter","0")
-sessionStorage.setItem("random_counter","0")
-
-sessionStorage.setItem("current_display_index","0");
-sessionStorage.setItem("questions_shown","");
 
  for (var i in data){
    var temp={};
@@ -196,7 +198,7 @@ sessionStorage.setItem("questions_shown","");
   all_data.push(temp)
 
  }
-
+//console.log(all_data)
 var quiz_only_data=[]
 for (var i in all_data){
 
@@ -204,7 +206,7 @@ for (var i in all_data){
 }
 
 
-play_random();
+  gen_html()
 
 //})
 })
@@ -212,7 +214,16 @@ play_random();
 }
 
 
+function get_previous(){
+  var all_questions = sessionStorage.getItem("all_questions").split(",")
+  var index = Number(sessionStorage.getItem("gone"))
+  get_next(all_questions[index-1])
+
+
+}
+
 function choose_index(direction,current_counter){
+
   var current_counter = sessionStorage.getItem("current_display_index");
 
 
@@ -222,12 +233,11 @@ function choose_index(direction,current_counter){
 
 
   var counter_array = _.map(counter_array_str, Number);
-  console.log(counter_array)
+
   //(counter_array)
   var index_to_show;
    var curr = Number(counter_array.indexOf(current_counter));
-   console.log(curr)
-   console.log(counter_array)
+
    var curr_arr_len = counter_array.length
    //(curr);
   // (curr_arr_len);
@@ -276,6 +286,8 @@ function get_next_q () {
   sessionStorage.setItem("questions_shown",questions_shown+","+removed[0]) //array of selected indices or removed item indices
   sessionStorage.setItem("current_display_index",removed)
   //(removed)
+  //console.log(questions_shown)
+  //console.log(removed)
    return removed; // is an array with one element, that is how splice works
 }
 
@@ -283,10 +295,14 @@ function get_next_q () {
 
 
 function play_random(){
+
+/*
       var next;var cbox_html=""; var string=""
       var next
       var dif=false;
 
+
+       var count = Number(sessionStorage.getItem("dif_counter"))
 
       if(arguments.length===0){
       next = get_next_q()[0] //this will be the ucis id minus 1 since the pointers start from 0 and the ucis ids start from 1. However the pointer and the id will be the same
@@ -303,14 +319,17 @@ function play_random(){
          var id = all_data[next]["id"]
       }
       else{
-
+        var dif = sessionStorage.getItem("difficult");
        if(count===0){
        sessionStorage.setItem("questions_not_shown",dif);
        }
        count = count+1
 
 
+
        next = get_next_q()[0]
+
+
         if(next.length===0) {
              next =0;// you are done
              $("#quiz_div").html("complete");
@@ -323,13 +342,13 @@ function play_random(){
       }
 
        var random_count = Number(sessionStorage.getItem("random_counter"));
-        console.log(`random_count `+random_count);
-      gen_html(next)
+*/
 
+/*
       random_count=random_count+1;
       sessionStorage.setItem("random_counter",String(random_count))
 
-
+*/
 
 }
 
@@ -341,9 +360,15 @@ function play_random(){
 
 function gen_html(index,direction,flag){
 
-  //sessionStorage.setItem("current_display_index",index)
+ var vals = sessionStorage.getItem("questions_not_shown").split(",")
+//console.log(vals)
+ vals.shift();
+var gone =vals[0]
+ sessionStorage.setItem("gone",gone)
 
-  var index = sessionStorage.getItem("current_display_index")
+ sessionStorage.setItem("questions_not_shown",vals.toString())
+
+ index=gone
 
   var ans = keys[index]["answer"]
   var q = keys[index]["question"]
@@ -352,8 +377,7 @@ function gen_html(index,direction,flag){
   var show_dropdown_state = keys[index]["is_location_dependent"]
   var current_counter = index
   document.getElementById("question").innerHTML = "Question: "+ucis_id+" "+q
-  //console.log(current_counter)
-  //console.log(ucis_id)
+
 
   $( "#show-answer" ).click(function() {
     $("#answer").html(ans)
